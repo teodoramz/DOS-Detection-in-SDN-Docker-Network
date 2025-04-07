bdc8e72a2f93e3260055ddee8663b5fddab46e9e
Teodor Amzuloiu <amzteodor@gmail.com>
2025-04-07 14:54:28 +0300

    Kafka + kafdrop

---
+++ ovs/install_docker.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Docker/Docker-compose installation
+
+sudo apt-get update
+
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+
+sudo apt-get install -y docker-ce
+
+sudo usermod -aG docker $USER
+
+docker --version
+
+
+DOCKER_COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
+sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker-compose --version
+
+echo "Installation completed!"
